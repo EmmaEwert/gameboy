b41688fd16cffeccdd5379adbe6d68a2991e61e1
Emma Ewert <elyon.itari@gmail.com>
2016-09-20 21:30:42 +0200

    Load tile data

--- main.asm
+++ main.asm
@@ -7,8 +7,8 @@ section "Header", rom0[$100]
   db    "TITLE      CODE",$80,0,0,0 ; Title&ID, GBC?, 2 license, SGB?
   db    0,0,0,0,$33 ; cart, rom, ram, region, old license
 
-section "LCD Status interrupt", rom0[$48]
-  jp HBlank
+section "LCD Status", rom0[$48]
+  reti
 
 section "Program", rom0[$150]
 Main
@@ -16,21 +16,32 @@ Main
   ldh   [$ff],a     ; enable LCD STAT interrupt
   ld    a,%00001000
   ldh   [$41],a     ; enable H-Blank interrupt
+  call  LoadTiles
   ei
 .Update
   halt
   jp    .Update
 
-HBlank
-  ld    c,$68
-  ld    a,%10000000 ; auto increment from palette 0 color 0
-  ld    [c],a
-  inc   c
-  ld    a,e
-  ld    [c],a
-  ld    a,d
-  ld    [c],a
-  inc   de
-  reti
+LoadTiles
+  ld    hl,$0800    ; ROM, swap(h) = RAM up to $80ff
+  ld    b,$10       ; $n0 = n tiles
+.while;b --> 0
+  ld    a,[hl]
+  swap  h
+  ldi   [hl],a
+  swap  h
+  dec   b
+  jp    nz,.while
+  ret
+
+section "Tile data", rom0[$800]
+  dw    `23311111
+  dw    `23311133
+  dw    `33311133
+  dw    `33111333
+  dw    `33111333
+  dw    `31113333
+  dw    `30013333
+  dw    `00013333
 
 ; vim:syn=rgbasm
