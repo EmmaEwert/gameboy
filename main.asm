d848d721248c2dde92cd983a3dd5953c4e24944e
Emma Ewert <elyon.itari@gmail.com>
2016-09-20 03:15:11 +0200

    Rename Start section to Header, move to top

--- main.asm
+++ main.asm
@@ -1,4 +1,13 @@
-section "LCD Status", rom0[$48] ; interrupt
+section "Header", rom0[$100]
+  nop
+  jp    Main
+  dw    $ceed,$6666,$cc0d,$000b,$0373,$0083,$000c,$000d ; Nintendo
+  dw    $0008,$111f,$8889,$000e,$dccc,$6ee6,$dddd,$d999 ; logo, exact
+  dw    $bbbb,$6763,$6e0e,$eccc,$dddc,$999f,$bbb9,$333e ; (required)
+  db    "TITLE      CODE",$80,0,0,0 ; Title&ID, GBC?, 2 license, SGB?
+  db    0,0,0,0,$33 ; cart, rom, ram, region, old license
+
+section "LCD Status interrupt", rom0[$48]
   ld    c,$68
   ld    a,%10000000 ; auto increment from palette 0 color 0
   ld    [c],a
@@ -11,14 +20,6 @@ section "LCD Status", rom0[$48] ; interrupt
   reti
   ;fixme: overflows well into next interrupt section
 
-section "Start", rom0[$101]
-  jp    Main
-  dw    $ceed,$6666,$cc0d,$000b,$0373,$0083,$000c,$000d ; Nintendo
-  dw    $0008,$111f,$8889,$000e,$dccc,$6ee6,$dddd,$d999 ; logo, exact
-  dw    $bbbb,$6763,$6e0e,$eccc,$dddc,$999f,$bbb9,$333e ; (required)
-  db    "TITLE      CODE",$80,0,0,0 ; Title&ID, GBC?, 2 license, SGB?
-  db    0,0,0,0,$33 ; cart, rom, ram, region, old license
-
 section "Program", rom0[$150]
 Main
   ld    a,%00000010
@@ -29,5 +30,4 @@ Main
 .Update
   halt
   jp    .Update
-
 ; vim:syn=rgbasm
