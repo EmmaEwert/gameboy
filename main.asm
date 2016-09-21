f09b65244c6021fc920d113418f57de930ccf1af
Emma Ewert <elyon.itari@gmail.com>
2016-09-21 03:20:39 +0200

    Add 3 tiles, use jr instruction where relevant

--- main.asm
+++ main.asm
@@ -28,29 +28,29 @@ Main
   ei
 .Update
   halt
-  jp    .Update
+  jr    .Update
 
 LoadTiles
   ld    hl,$0800    ; ROM, swap(h) = RAM up to $80ff
-  ld    b,$20       ; $n0 = n tiles
+  ld    b,$50       ; $n0 = n tiles
 .while;b --> 0
   ld    a,[hl]
   swap  h
   ld    [hl+],a
   swap  h
   dec   b
-  jp    nz,.while
+  jr    nz,.while
   ret
 
 LoadMap
   ld    hl,$9800    ; VRAM background map
   xor   a
-  ld    b,$2;tiles
+  ld    b,$5;tiles
 .while;b --> 0
   ld    [hl+],a
   inc   a
   dec   b
-  jp    nz,.while
+  jr    nz,.while
   ret
 
 LoadPalettes
@@ -59,12 +59,12 @@ LoadPalettes
   ld    [c],a
   inc   c
   ld    hl,$0700    ; ROM
-  ld    b,$10;bytes, 2 per color = 8 colors
+  ld    b,$10;bytes, 2 per color = 8 colors = 2 palettes
 .while;b --> 0
   ld    a,[hl+]
   ld    [c],a
   dec   b
-  jp    nz,.while
+  jr    nz,.while
   ret
 
 RefreshPalette
@@ -77,9 +77,7 @@ RefreshPalette
   ld    de,%0001100010100110
 .wait
   bit   1,[hl]
-  jp    nz,.wait
-  bit   0,[hl]
-  jp    nz,.wait
+  jr    nz,.wait
   ld    a,e
   ld    [c],a
   ld    a,d
@@ -88,17 +86,45 @@ RefreshPalette
 
 section "Palette data", rom0[$700]
         ; BBBBBGGGGGRRRRR
-  dw    %0010000011100111 ; #383840
-  dw    %0010100100001000 ; #404050
-  dw    %0011110110001101 ; #686078
-  dw    %0010110100101010 ; #504858
-
   dw    %0100101001010000 ; #809090
   dw    %0011110111101110 ; #707878
   dw    %0011110110001101 ; #686078
   dw    %0010110100101010 ; #504858
 
+  dw    %0010000011100111 ; #383840
+  dw    %0010100100001000 ; #404050
+  dw    %0011110110001101 ; #686078
+  dw    %0010110100101010 ; #504858
+
+
 section "Tile data", rom0[$800]
+  dw    `00000000
+  dw    `00000000
+  dw    `00000000
+  dw    `00000000
+  dw    `00000000
+  dw    `10000000
+  dw    `11000001
+  dw    `00011001
+
+  dw    `00011111
+  dw    `00011111
+  dw    `00111111
+  dw    `00111111
+  dw    `11111111
+  dw    `11111112
+  dw    `11111112
+  dw    `11111112
+
+  dw    `12222222
+  dw    `12222222
+  dw    `22222222
+  dw    `22222222
+  dw    `22222223
+  dw    `22222223
+  dw    `22332233
+  dw    `23333333
+
   dw    `23311111
   dw    `23311133
   dw    `33311133
