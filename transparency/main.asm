0c29d42a3ec4d4802e74a2cd87dc9c48a65a47d1
Emma Ewert <EmmaEwert@users.noreply.github.com>
2016-10-19 16:07:10 +0200

    Display text in window

--- transparency/main.asm
+++ transparency/main.asm
@@ -19,7 +19,119 @@ section "Header", rom0[$0100]
 
 
 
+section "Data", romx,bank[1]
+WINDOW_ATTRIBUTES set 0
+WindowAttributes:
+                rept  $400
+                db    %10001001
+WINDOW_ATTRIBUTES set WINDOW_ATTRIBUTES+1
+                endr
+WINDOW_MAP      set   0
+WindowMap:      db    $40, $40, $40, $40, $00, $01, $02, $03
+                db    $40, $40, $40, $40, $40, $40, $40, $40
+                db    $40, $40, $40, $40, $40, $40, $40, $40
+                db    $40, $40, $40, $40, $40, $40, $40, $40
+WINDOW_MAP      set   WINDOW_MAP+$20
+                db    $40, $40, $40, $40, $10, $11, $12, $13
+                db    $40, $40, $40, $40, $40, $40, $40, $40
+                db    $40, $40, $40, $40, $40, $40, $40, $40
+                db    $40, $40, $40, $40, $40, $40, $40, $40
+WINDOW_MAP      set   WINDOW_MAP+$20
+                rept  $20-(WINDOW_MAP/$20)
+                db    $40, $40, $40, $40, $40, $40, $40, $40
+                db    $40, $40, $40, $40, $40, $40, $40, $40
+                db    $40, $40, $40, $40, $40, $40, $40, $40
+                db    $40, $40, $40, $40, $40, $40, $40, $40
+WINDOW_MAP      set   WINDOW_MAP+$20
+                endr
+
+TILES           set   0
+TEXT_TILES      set   0
+TextTiles:      dw    `00000000
+                dw    `00000000
+                dw    `00000000
+                dw    `00000000
+                dw    `33100000
+                dw    `30301320
+                dw    `33103030
+                dw    `30003030
+TEXT_TILES      set   TEXT_TILES+1
+                dw    `00000000
+                dw    `00000000
+                dw    `00000020
+                dw    `00000300
+                dw    `30000000
+                dw    `30001310
+                dw    `30303030
+                dw    `33003330
+TEXT_TILES      set   TEXT_TILES+1
+                dw    `00000000
+                dw    `00000000
+                dw    `00000000
+                dw    `00000000
+                dw    `00000000
+                dw    `33131013
+                dw    `31313030
+                dw    `30303030
+TEXT_TILES      set   TEXT_TILES+1
+                dw    `00000000
+                dw    `00000000
+                dw    `00000000
+                dw    `00000000
+                dw    `00000000
+                dw    `20332000
+                dw    `30303000
+                dw    `30303000
+TEXT_TILES      set   TEXT_TILES+1
+                rept  $10-$04
+                rept  $08
+                dw    `00000000
+                endr
+TEXT_TILES      set   TEXT_TILES+1
+                endr
+                dw    `30003030
+                dw    `30002310
+                dw    `00000000
+                dw    `00000000
+                dw    `00000000
+                dw    `00000000
+                dw    `00000000
+                dw    `00000000
+TEXT_TILES      set   TEXT_TILES+1
+                dw    `30203000
+                dw    `30301330
+                dw    `00000000
+                dw    `00000000
+                dw    `00000000
+                dw    `00000000
+                dw    `00000000
+                dw    `00000000
+TEXT_TILES      set   TEXT_TILES+1
+                dw    `30303030
+                dw    `30303023
+                dw    `00000000
+                dw    `00000000
+                dw    `00000000
+                dw    `00000000
+                dw    `00000000
+                dw    `00000000
+TEXT_TILES      set   TEXT_TILES+1
+                dw    `30303000
+                dw    `10303000
+                dw    `00000000
+                dw    `00000000
+                dw    `00000000
+                dw    `00000000
+                dw    `00000000
+                dw    `00000000
+TEXT_TILES      set   TEXT_TILES+1
+TILES           set   TILES+TEXT_TILES
+
+
+
+
 section "V-Blank interrupt vector", rom0[$40]
+                call  SwapBuffer
                 reti
 
 
@@ -32,22 +144,104 @@ Main:           call Setup
 
 
 
+section "Code", rom0;      ┌──┬LCD Control
+SwapBuffer:     ld    hl, $ff40;┌Window Display enabled?
+                ld    a,     %00100000
+                xor   [hl]
+                ld    [hl], a
+;                          ┌──┬Window Y Position
+                ld    hl, $ff4a
+                ld    [hl], $90-$20
+;                          ┌──┬Window X Position minus 7
+                ld    hl, $ff4b
+                ld    [hl], $07
+                ret
+
+
+
 section "Setup", rom0
-Setup:;                    ┌──┬LY register
+Setup:;                    ┌──┬LY
 .disableLCD:    ld    hl, $ff44;┌┬Line 144, V-Blank starts
                 ld    a,       $90
 .ltLY144:       cp    [hl]
                 jr    C, @+2
                 jr    NZ, .ltLY144
+;                          ┌──┬LCD Control
+                ld    hl, $ff40;┌LCD Display enabled?
+                res             7, [hl]
 ;                          ┌──┬Prepare Speed Switch, CGB Mode Only
 .switchSpeed:   ld    hl, $ff4d;┌Prepare Speed Switch?
                 set             0, [hl]
-                stop;      ┌──┬LCD Control
-.enableLCD:     ld    hl, $ff40;┌LCD Display enabled?
-                set             7, [hl]
-.enableInterrupts:;        ┌──┬Interrupt Enable register
+                stop
+;                          ┌──┬Background Palette Index
+.setPalettes:   ld    hl, $ff68;┌Auto Increment?
+                ld    [hl],    %10000000
+;                          ┌──┬Background Palette Data
+                inc   l;  $ff69
+                ld    [hl], %10001000;Palette 0
+                ld    [hl], %01000000
+                ld    [hl], %00000000
+                ld    [hl], %00000000
+                ld    [hl], %00000000
+                ld    [hl], %00000000
+                ld    [hl], %00000000
+                ld    [hl], %00000000
+                ld    [hl], %00000000;Palette 1
+                ld    [hl], %00000000
+                ld    [hl], %00001000
+                ld    [hl], %00100001
+                ld    [hl], %00010000
+                ld    [hl], %01000010
+                ld    [hl], %11111111
+                ld    [hl], %01111111
+;                          ┌──┬LCD VRAM Bank
+.writeTextTiles:ld    hl, $ff4f;┌VRAM Bank = 1
+                set             0, [hl]
+                ld    hl, $ff51
+                ld    [hl], TextTiles/$100
+                inc   l
+                ld    [hl], TextTiles%$100
+                inc   l
+                ld    [hl], $80
+                inc   l
+                ld    [hl], $00
+                inc   l
+                ld    [hl], (TEXT_TILES-1)
+.writeWindowAttributes:
+                ld    hl, $ff51
+                ld    [hl], WindowAttributes/$100
+                inc   l
+                ld    [hl], WindowAttributes%$100
+                inc   l
+                ld    [hl], $9c
+                inc   l
+                ld    [hl], $00
+                inc   l
+                ld    [hl], (WINDOW_ATTRIBUTES/$10-1)
+;                          ┌──┬LCD VRAM Bank
+                ld    hl, $ff4f;┌VRAM Bank = 0
+                res             0, [hl]
+.writeWindowMap:ld    hl, $ff51
+                ld    [hl], WindowMap/$100
+                inc   l
+                ld    [hl], WindowMap%$100
+                inc   l
+                ld    [hl], $9c
+                inc   l
+                ld    [hl], $00
+                inc   l
+                ld    [hl], (WINDOW_MAP/$10-1)
+;                          ┌──┬LCD Control
+.selectWindow:  ld    hl, $ff40;┌Window Tile Map Display Select = $9c00-$9fff
+                set             6, [hl]
+;                     ┌LCD Display enabled?
+.enableLCD:     set   7, [hl]
+.enableInterrupts:;        ┌──┬Interrupt Enable
                 ld    hl, $ffff;┌V-Blank interrupt enabled?
                 set             0, [hl]
                 reti
+
+
+
 
 ; vim:filetype=rgbasm expandtab softtabstop=2
