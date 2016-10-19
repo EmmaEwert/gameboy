0f014919f4333e99c1cf67e1d52e54ef09467a72
Emma Ewert <EmmaEwert@users.noreply.github.com>
2016-10-19 06:51:55 +0200

    Write repeated colors in color counter to png file

--- tools/colors.go
+++ tools/colors.go
@@ -15,21 +15,25 @@ func main() {
   }
   file, _ := os.Open(os.Args[1])
   image, _ := png.Decode(file)
-  count := countColors(image)
-  fmt.Printf("%d unique colors\n", count)
+  count, out := countColors(image)
+  fmt.Printf("%d unique colors\n%d repeat colors, saved in %s\n", count, image.Bounds().Max.X * image.Bounds().Max.Y - count, out)
 }
 
-func countColors(image image.Image) int {
-  max := image.Bounds().Max
+func countColors(in image.Image) (int, string) {
+  var encoder png.Encoder
+  encoder.CompressionLevel = png.BestCompression
+  max := in.Bounds().Max
+  out := image.NewRGBA(image.Rect(0, 0, max.X, max.Y))
   count := 0
   var colors = make([]color.Color, max.X * max.Y)
   for i, _ := range colors {
     x := i % max.X
     y := i / max.Y
-    color := image.At(x, y)
+    color := in.At(x, y)
     c := count+1
     for c >= 0 {
       if colors[c] == color {
+        out.Set(x, y, color)
         break
       }
       c--
@@ -39,5 +43,9 @@ func countColors(image image.Image) int {
       count++
     }
   }
-  return count
+  filename := "repeats.png"
+  file, _ := os.Create(filename)
+  defer file.Close()
+  encoder.Encode(file, out)
+  return count, filename
 }
